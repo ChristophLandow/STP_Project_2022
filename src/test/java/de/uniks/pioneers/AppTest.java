b0f28fb4787bfe8d7b017fce60e0806a1381fa30
Aleyna Albayrak <uk076901@student.uni-kassel.de>
2022-04-26 23:23:23 +0200

    test: add App test

---
+++ src/test/java/de/uniks/pioneers/AppTest.java
@@ -0,0 +1,14 @@
+package de.uniks.pioneers;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void test() {
+
+    }
+
+}
