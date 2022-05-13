d2f4f4b985682ea08ca9fc4798d126126768eb90
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2022-05-13 17:05:55 +0200

    chore: completed LoginScreenControllerTest

--- src/test/java/de/uniks/pioneers/controller/LoginScreenControllerTest.java
+++ src/test/java/de/uniks/pioneers/controller/LoginScreenControllerTest.java
@@ -1,12 +1,58 @@
 package de.uniks.pioneers.controller;
 
 import de.uniks.pioneers.App;
+import de.uniks.pioneers.model.LoginResult;
 import de.uniks.pioneers.services.LoginService;
+import de.uniks.pioneers.services.PrefService;
+import io.reactivex.rxjava3.core.Observable;
+import javafx.scene.input.KeyCode;
+import javafx.stage.Stage;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.testfx.api.FxAssert;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.control.TextMatchers;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-import javax.inject.Provider;
+@ExtendWith(MockitoExtension.class)
+class LoginScreenControllerTest extends ApplicationTest {
 
-import static org.junit.jupiter.api.Assertions.*;
+    @Spy
+    App app;
+    @Mock
+    LoginService loginService;
+    @Mock
+    PrefService prefService;
 
-class LoginScreenControllerTest {
+    @InjectMocks
+    LoginScreenController loginScreenController;
+    @Override
+    public void start(Stage stage){
+
+        when(prefService.recall()).thenReturn("");
+
+        app.start(stage);
+        app.show(loginScreenController);
+    }
+    @Test
+    public void login(){
+
+        when(loginService.login("Test","12345678")).thenReturn(Observable.just(new LoginResult("000","Test","online","","accessToken","refreshToken")));
+
+        FxAssert.verifyThat("#userNameStatusText", TextMatchers.hasText("Please enter a valid user name"));
+        write("Test\t");
+        FxAssert.verifyThat("#userNameStatusText", TextMatchers.hasText(""));
+        write("12345678\t");
+        type(KeyCode.SPACE);
+        write("\t");
+        type(KeyCode.SPACE);
+
+        verify(loginService).login("Test","12345678");
+
+    }
 }
