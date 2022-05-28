284a4ae69b6be6ed8836183630bcec71862857da
Christoph Landow <clandow@web.de>
2022-05-28 09:14:03 +0200

    feat : Implemented SettingsScreenController and title in ConstantsClass

---
+++ src/main/java/de/uniks/pioneers/controller/SettingsScreenController.java
@@ -0,0 +1,22 @@
+package de.uniks.pioneers.controller;
+
+import javafx.scene.Parent;
+import static de.uniks.pioneers.Constants.INGAME_SCREEN_TITLE;
+
+public class SettingsScreenController implements Controller{
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
+    }
+}
