03402b3a8c8c71fe2b58362e1aaf617571083a51
Ferdinand Blum <uk044779@student.uni-kasssel.de>
2022-06-04 20:02:04 +0200

    feat: street sub controller

--- src/main/java/de/uniks/pioneers/controller/subcontroller/StreetPointController.java
+++ src/main/java/de/uniks/pioneers/controller/subcontroller/StreetPointController.java
@@ -1,7 +1,11 @@
 package de.uniks.pioneers.controller.subcontroller;
 
+import de.uniks.pioneers.dto.CreateBuildingDto;
+import de.uniks.pioneers.dto.CreateMoveDto;
 import de.uniks.pioneers.model.ExpectedMove;
 import de.uniks.pioneers.services.GameStorage;
+import de.uniks.pioneers.services.IngameService;
+import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
@@ -9,19 +13,24 @@ import javafx.scene.shape.Circle;
 import javax.inject.Inject;
 import java.util.ArrayList;
 
+import static de.uniks.pioneers.Constants.FX_SCHEDULER;
 import static de.uniks.pioneers.GameConstants.*;
 
 public class StreetPointController {
 
     private final GameStorage gameStorage;
+    private final IngameService ingameService;
     public HexTile tile;
     private Circle view;
+
+    private final CompositeDisposable disposable = new CompositeDisposable();
 
     public ArrayList<BuildingPointController> buildings = new ArrayList<>();
 
     @Inject
-    public StreetPointController(GameStorage gameStorage) {
+    public StreetPointController(GameStorage gameStorage, IngameService ingameService) {
         this.gameStorage = gameStorage;
+        this.ingameService = ingameService;
     }
 
     public void post(HexTile tile, Circle view) {
@@ -42,7 +51,14 @@ public class StreetPointController {
 
         if (move.players().get(0).equals(gameStorage.currentPlayer.userId())) {
             if ((move.action().equals(FOUNDING_ROAD_1) || move.action().equals(FOUNDING_ROAD_2))) {
-                buildings.stream().anyMatch(c -> gameStorage.checkRoadSpot(c.tile.q,c.tile.r,c.tile.s));
+                if (buildings.stream().anyMatch(c -> gameStorage.checkRoadSpot(c.tile.q,c.tile.r,c.tile.s))){
+                    CreateBuildingDto newBuilding = new CreateBuildingDto(tile.q, tile.r, tile.s, 6, "street");
+                    disposable.add(ingameService.postMove(gameId, new CreateMoveDto(move.action(), newBuilding))
+                            .observeOn(FX_SCHEDULER)
+                            .subscribe(move -> {
+
+                            }));
+                };
             }
             //render street main phase
 
