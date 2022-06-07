3d8bd02d571e2074fe5c4e2c1cb69402613c8d77
Ferdinand Blum <uk044779@student.uni-kasssel.de>
2022-06-07 16:19:30 +0200

    feat: ingame Player list

--- src/main/java/de/uniks/pioneers/controller/subcontroller/IngamePlayerListElementController.java
+++ src/main/java/de/uniks/pioneers/controller/subcontroller/IngamePlayerListElementController.java
@@ -2,7 +2,6 @@ package de.uniks.pioneers.controller.subcontroller;
 
 
 import de.uniks.pioneers.Main;
-import de.uniks.pioneers.model.Member;
 import de.uniks.pioneers.model.Player;
 import de.uniks.pioneers.model.Resources;
 import de.uniks.pioneers.model.User;
@@ -25,37 +24,49 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Circle;
-import javafx.scene.shape.SVGPath;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.Objects;
 
 import static de.uniks.pioneers.Constants.FX_SCHEDULER;
 
 public class IngamePlayerListElementController {
 
-
-
+    @FXML
+    public HBox playerBox;
+    @FXML
+    public Circle playerColor;
+    @FXML
+    public ImageView playerAvatar;
+    @FXML
+    public Label resourceCardsCount;
+    @FXML
+    public ImageView resourceCards;
+    @FXML
+    public Label developmentCardsCount;
+    @FXML
+    public ImageView developmentCards;
+    @FXML
+    public Label settlementCount;
+    @FXML
+    public ImageView settlement;
+    @FXML
+    public Label cityCount;
+    @FXML
+    public ImageView city;
+    @FXML
+    public Label playerName;
+    @FXML
+    private Player toRender;
+    @FXML
+    public ListView<Node> nodeListView;
 
     private final CompositeDisposable disposable = new CompositeDisposable();
     private final GameStorage gameStorage;
     private final UserService userService;
     private final EventListener eventListener;
-    boolean online;
-    @FXML public HBox playerBox;
-    @FXML public Circle playerColor;
-    @FXML public ImageView playerAvatar;
-    @FXML public Label resourceCardsCount;
-    @FXML public ImageView resourceCards;
-    @FXML public Label devolopmentCardsCount;
-    @FXML public ImageView developmentCards;
-    @FXML public Label settlementCount;
-    @FXML public ImageView settlement;
-    @FXML public Label cityCount;
-    @FXML public ImageView city;
-    @FXML private Player toRender;
-    @FXML public ListView<Node> nodeListView;
-
+    private boolean online;
 
     @Inject
     public IngamePlayerListElementController(GameStorage gameStorage, UserService userService, EventListener eventListener) {
@@ -64,7 +75,7 @@ public class IngamePlayerListElementController {
         this.eventListener = eventListener;
     }
 
-    public void stop(){
+    public void stop() {
         disposable.dispose();
     }
 
@@ -79,54 +90,56 @@ public class IngamePlayerListElementController {
             e.printStackTrace();
         }
 
+        // set values to gui and setup listeners
         toRender = gameStorage.players.get(playerId);
-        playerBox.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
-
-        // set valures to gui
-        setImages();
         playerColor.setFill(Paint.valueOf(toRender.color()));
         disposable.add(userService.getUserById(toRender.userId())
                 .observeOn(FX_SCHEDULER)
                 .subscribe(user -> {
-                    playerAvatar = new ImageView(new Image(user.avatar()));
+                    playerAvatar.setImage(new Image(user.avatar()));
+                    playerName.setText(user.name());
                     addUserListener(user._id());
                 })
         );
 
+        setDataToElement(toRender);
+        setImages();
         addPlayerListener();
     }
 
     private void setImages() {
         // set values to gui elements
-        Image resourceImage = new Image(getClass().getResource("images/card_question_mark.png").toString());
+        Image resourceImage = new Image(Objects.requireNonNull(getClass().getResource("images/card_question_mark.png")).toString());
         resourceCards.setImage(resourceImage);
-        Image developmentImage = new Image(getClass().getResource("images/card_hammer.png").toString());
+        Image developmentImage = new Image(Objects.requireNonNull(getClass().getResource("images/card_hammer.png")).toString());
         developmentCards.setImage(developmentImage);
-        Image cityImage = new Image(getClass().getResource("images/steine_3.png").toString());
+        Image cityImage = new Image(Objects.requireNonNull(getClass().getResource("images/steine_3.png")).toString());
         city.setImage(cityImage);
-        Image settlementImage = new Image(getClass().getResource("images/ruinsCorner.png").toString());
+        Image settlementImage = new Image(Objects.requireNonNull(getClass().getResource("images/ruinsCorner.png")).toString());
         settlement.setImage(settlementImage);
     }
 
+    // have to fix style
     private void addUserListener(String id) {
-            String patternToObserveUser = String.format("users.%s.updated", id);
-            disposable.add(eventListener.listen(patternToObserveUser, User.class)
-                    .observeOn(FX_SCHEDULER)
-                    .subscribe(userEvent -> {
-                        User userFromEvent = userEvent.data();
-                        if (userFromEvent.status().equals("offline")) {
-                            // should be changed
-                            playerBox.setBackground(new Background(new BackgroundFill(Color.TOMATO, null, null)));
-                            playerBox.setOpacity(40);
-                            online=false;
-                        }else if (userFromEvent.status().equals("online") && !online){
-                            playerBox.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
-                            playerBox.setOpacity(100);
-                            online=true;
-                        }
-                    })
-            );
-        }
+        String patternToObserveUser = String.format("users.%s.updated", id);
+        disposable.add(eventListener.listen(patternToObserveUser, User.class)
+                .observeOn(FX_SCHEDULER)
+                .subscribe(userEvent -> {
+                    User userFromEvent = userEvent.data();
+                    Background background = new Background(new BackgroundFill(Color.WHITE, null, null));
+                    if (userFromEvent.status().equals("offline")) {
+                        background = new Background(new BackgroundFill(Color.TOMATO, null, null));
+                        playerBox.setBackground(background);
+                        playerBox.setOpacity(40);
+                        online = false;
+                    } else if (userFromEvent.status().equals("online") && !online) {
+                        playerBox.setBackground(background);
+                        playerBox.setOpacity(100);
+                        online = true;
+                    }
+                })
+        );
+    }
 
 
     private void addPlayerListener() {
@@ -145,15 +158,17 @@ public class IngamePlayerListElementController {
 
     private void setDataToElement(Player valueAdded) {
         Resources resources = valueAdded.resources();
-        int resoureceCount = resources.brick()+ resources.grain()+ resources.ore()+ resources.lumber()+ resources.wool();
-        resourceCardsCount.setText(String.valueOf(resoureceCount));
-        devolopmentCardsCount.setText(String.valueOf(resources.unknown()));
-
-
-
+        int resourceCount;
+        try {
+            resourceCount = resources.brick() + resources.grain() + resources.ore() + resources.lumber() + resources.wool();
+        } catch (NullPointerException e) {
+            resourceCount = 0;
+        }
+        resourceCardsCount.setText(String.valueOf(resourceCount));
+        developmentCardsCount.setText(String.valueOf(resources.unknown()));
+        cityCount.setText(String.valueOf(4 - valueAdded.remainingBuildings().city()));
+        settlementCount.setText(String.valueOf(5 - valueAdded.remainingBuildings().settlement()));
     }
-
-
 }
 
 
