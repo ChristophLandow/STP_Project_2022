370e7ed9f8a9d748ffe4ba730ea7ef00b8ceb574
Christoph Landow <clandow@web.de>
2022-06-09 16:55:09 +0200

    fix:  processed warnings

--- src/test/java/de/uniks/pioneers/controller/SettingsControllerTest.java
+++ src/test/java/de/uniks/pioneers/controller/SettingsControllerTest.java
@@ -1,35 +1,22 @@
 package de.uniks.pioneers.controller;
 
 import de.uniks.pioneers.App;
-
-
 import javafx.stage.Stage;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.testfx.api.FxAssert;
 import org.testfx.framework.junit5.ApplicationTest;
 import org.testfx.matcher.base.NodeMatchers;
 
-import javax.inject.Provider;
-
 
 
 @ExtendWith(MockitoExtension.class)
 public class SettingsControllerTest extends ApplicationTest {
-
     @Spy
     App app = new App(null);
-
-    @Mock
-    IngameScreenController ingameScreenController;
-
-    @Mock(name = "ingameScreenControllerProvider")
-    Provider<IngameScreenController> ingameScreenControllerProvider;
 
     @InjectMocks
     SettingsScreenController settingsScreenController;
