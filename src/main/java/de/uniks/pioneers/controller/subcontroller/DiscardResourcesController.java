ec451506922a8d05358a939735a22a7a9eb4801f
Christoph Landow <clandow@web.de>
2022-06-22 18:03:59 +0200

    feat: DiscardResources Subcontroller

---
+++ src/main/java/de/uniks/pioneers/controller/subcontroller/DiscardResourcesController.java
@@ -0,0 +1,65 @@
+package de.uniks.pioneers.controller.subcontroller;
+
+import de.uniks.pioneers.App;
+import de.uniks.pioneers.Main;
+import de.uniks.pioneers.controller.Controller;
+import de.uniks.pioneers.services.GameService;
+import de.uniks.pioneers.services.IngameService;
+import de.uniks.pioneers.services.PrefService;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.Spinner;
+import javafx.scene.text.Text;
+
+import javax.inject.Inject;
+import java.io.IOException;
+
+public class DiscardResourcesController implements Controller {
+    @Inject
+    IngameService ingameService;
+    @Inject
+    GameService gameService;
+    @Inject
+    PrefService prefService;
+    @FXML Text XfromSixText;
+    @FXML Text SlashText;
+    @FXML Text SixText;
+    @FXML Spinner<Integer> CarbonSpinner;
+    @FXML Spinner<Integer> FishSpinner;
+    @FXML Spinner<Integer> PolarBearSpinner;
+    @FXML Spinner<Integer> IceSpinner;
+    @FXML Spinner<Integer> WaleSpinner;
+    private App app;
+
+    @Override
+    public void init() {
+        if(prefService.getDarkModeState()){
+            this.app.getStage().getScene().getStylesheets().removeIf((style -> style.equals("/de/uniks/pioneers/styles/DiscardResourcesPopup.css")));
+            this.app.getStage().getScene().getStylesheets().add( "/de/uniks/pioneers/styles/DarkMode_DiscardResourcesPopup.css");
+        } else {
+            this.app.getStage().getScene().getStylesheets().removeIf((style -> style.equals("/de/uniks/pioneers/styles/DarkMode_DiscardResourcesPopup.css")));
+            this.app.getStage().getScene().getStylesheets().add( "/de/uniks/pioneers/styles/DiscardResourcesPopup.css");
+        }
+        
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Parent render() {
+        final FXMLLoader loader = new FXMLLoader(Main.class.getResource("views/DiscardResources.fxml"));
+        loader.setControllerFactory(c -> this);
+        final Parent parent;
+        try {
+            parent = loader.load();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return parent;
+    }
+}
--- src/main/java/de/uniks/pioneers/controller/subcontroller/RobberController.java
+++ src/main/java/de/uniks/pioneers/controller/subcontroller/RobberController.java
@@ -2,18 +2,24 @@ package de.uniks.pioneers.controller.subcontroller;
 
 
 import de.uniks.pioneers.App;
+import de.uniks.pioneers.Main;
+import de.uniks.pioneers.controller.Controller;
 import de.uniks.pioneers.dto.CreateMoveDto;
 import de.uniks.pioneers.services.GameService;
 import de.uniks.pioneers.services.IngameService;
 import de.uniks.pioneers.services.PrefService;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.input.MouseEvent;
 
 import javax.inject.Inject;
 
+import java.io.IOException;
+
 import static de.uniks.pioneers.Constants.FX_SCHEDULER;
 
-public class RobberController {
+public class RobberController implements Controller {
 
     @Inject
     IngameService ingameService;
@@ -34,16 +40,21 @@ public class RobberController {
         this.app = app;
     }
 
+    @Override
     public void init(){
         discard();
         itsRobbingTime();
-        if(prefService.getDarkModeState()){
-            this.app.getStage().getScene().getStylesheets().removeIf((style -> style.equals("/de/uniks/pioneers/styles/DiscardResourcesPopup.css")));
-            this.app.getStage().getScene().getStylesheets().add( "/de/uniks/pioneers/styles/DarkMode_DiscardResourcesPopup.css");
-        } else {
-            this.app.getStage().getScene().getStylesheets().removeIf((style -> style.equals("/de/uniks/pioneers/styles/DarkMode_DiscardResourcesPopup.css")));
-            this.app.getStage().getScene().getStylesheets().add( "/de/uniks/pioneers/styles/DiscardResourcesPopup.css");
-        }
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public Parent render() {
+        return null;
     }
 
     private void discard() {
