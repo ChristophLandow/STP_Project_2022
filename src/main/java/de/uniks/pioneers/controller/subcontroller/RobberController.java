5b2b1f36bf719022270c3becdc77c16d0cd25920
Christoph Landow <clandow@web.de>
2022-06-22 15:35:19 +0200

    feat: added Darkmode for DiscardResources Popup

--- src/main/java/de/uniks/pioneers/controller/subcontroller/RobberController.java
+++ src/main/java/de/uniks/pioneers/controller/subcontroller/RobberController.java
@@ -1,9 +1,11 @@
 package de.uniks.pioneers.controller.subcontroller;
 
 
+import de.uniks.pioneers.App;
 import de.uniks.pioneers.dto.CreateMoveDto;
 import de.uniks.pioneers.services.GameService;
 import de.uniks.pioneers.services.IngameService;
+import de.uniks.pioneers.services.PrefService;
 import io.reactivex.rxjava3.disposables.CompositeDisposable;
 import javafx.scene.input.MouseEvent;
 
@@ -17,18 +19,31 @@ public class RobberController {
     IngameService ingameService;
     @Inject
     GameService gameService;
+
+    @Inject
+    PrefService prefService;
     private final HexTile tile;
+
+    private final App app;
     private String action;
     private final CompositeDisposable disposable = new CompositeDisposable();
 
-    public RobberController(HexTile tile, String action){
+    public RobberController(App app, HexTile tile, String action){
         this.tile = tile;
         this.action = action;
+        this.app = app;
     }
 
     public void init(){
         discard();
         itsRobbingTime();
+        if(prefService.getDarkModeState()){
+            this.app.getStage().getScene().getStylesheets().removeIf((style -> style.equals("/de/uniks/pioneers/styles/DiscardResourcesPopup.css")));
+            this.app.getStage().getScene().getStylesheets().add( "/de/uniks/pioneers/styles/DarkMode_DiscardResourcesPopup.css");
+        } else {
+            this.app.getStage().getScene().getStylesheets().removeIf((style -> style.equals("/de/uniks/pioneers/styles/DarkMode_DiscardResourcesPopup.css")));
+            this.app.getStage().getScene().getStylesheets().add( "/de/uniks/pioneers/styles/DiscardResourcesPopup.css");
+        }
     }
 
     private void discard() {
