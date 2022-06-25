6be1fdf30e0dc014fe6a97c443a3028270de7bd5
Ferdinand Blum <uk044779@student.uni-kasssel.de>
2022-06-25 13:02:06 +0200

    feat/ trade with bank

--- src/main/java/de/uniks/pioneers/controller/PopUpController/TradePopUpController.java
+++ src/main/java/de/uniks/pioneers/controller/PopUpController/TradePopUpController.java
@@ -2,12 +2,14 @@ package de.uniks.pioneers.controller.PopUpController;
 
 
 import de.uniks.pioneers.Main;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -15,50 +17,92 @@ import javafx.scene.layout.VBox;
 import javax.inject.Inject;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
 
 
 public class TradePopUpController {
-    @FXML public AnchorPane root;
-    @FXML public HBox subRoot;
-    @FXML public VBox tradeBox;
-    @FXML public HBox timerBox;
-    @FXML public Label timer;
-    @FXML public HBox offerBox;
-    @FXML public Label offer;
-    @FXML public HBox offerImages;
-    @FXML public ImageView packeisResource;
-    @FXML public ImageView fellResource;
-    @FXML public ImageView fischResource;
-    @FXML public ImageView kohleResource;
-    @FXML public ImageView walknochenResource;
-    @FXML public HBox spinnerBox;
-    @FXML public Spinner packeisSpinner;
-    @FXML public Spinner fellSpinner;
-    @FXML public Spinner fischSpinner;
-    @FXML public Spinner kohleSpinner;
-    @FXML public Spinner walknochenSpinner;
-    @FXML public HBox getBox;
-    @FXML public Label get;
-    @FXML public ImageView packeisResourceI;
-    @FXML public ImageView fellResourceI;
-    @FXML public ImageView fischResourceI;
-    @FXML public ImageView kohleResourceI;
-    @FXML public ImageView walknochenResourceI;
-    @FXML public HBox spinnerBoxI;
-    @FXML public Spinner packeisSpinnerI;
-    @FXML public Spinner fellSpinnerI;
-    @FXML public Spinner fischSpinnerI;
-    @FXML public Spinner kohleSpinnerI;
-    @FXML public Spinner walknochenSpinnerI;
-    @FXML public HBox tradeButtons;
-    @FXML public Button cancel;
-    @FXML public Button offerToPlayers;
-    @FXML public Button tradeWithBank;
-    @FXML public VBox playerListBox;
-    @FXML public ListView playerList;
+    @FXML
+    public AnchorPane root;
+    @FXML
+    public HBox subRoot;
+    @FXML
+    public VBox tradeBox;
+    @FXML
+    public HBox timerBox;
+    @FXML
+    public Label timer;
+    @FXML
+    public HBox offerBox;
+    @FXML
+    public Label offer;
+    @FXML
+    public HBox offerImages;
+    @FXML
+    public ImageView packeisResource;
+    @FXML
+    public ImageView fellResource;
+    @FXML
+    public ImageView fischResource;
+    @FXML
+    public ImageView kohleResource;
+    @FXML
+    public ImageView walknochenResource;
 
-    private SpinnerValueFactory<Integer> spinnerValueFactory;
+
+    @FXML
+    public HBox getBox;
+    @FXML
+    public Label get;
+    @FXML
+    public ImageView packeisResourceI;
+    @FXML
+    public ImageView fellResourceI;
+    @FXML
+    public ImageView fischResourceI;
+    @FXML
+    public ImageView kohleResourceI;
+    @FXML
+    public ImageView walknochenResourceI;
+
+
+    @FXML
+    public HBox tradeButtons;
+    @FXML
+    public Button cancel;
+    @FXML
+    public Button offerToPlayers;
+    @FXML
+    public Button tradeWithBank;
+    @FXML
+    public VBox playerListBox;
+    @FXML
+    public ListView playerList;
+
+    @FXML
+    public HBox spinnerBoxOffer;
+    @FXML
+    public HBox spinnerBoxGet;
+
+
+    private class TradeSpinnerFactory extends SpinnerValueFactory<Integer> {
+
+        @Override
+        public void decrement(int steps) {
+
+            if (getValue() > 0) {
+                setValue(getValue() - 1);
+            }
+        }
+
+        @Override
+        public void increment(int steps) {
+
+            setValue(getValue() + 1);
+        }
+    }
 
 
     @Inject
@@ -79,42 +123,29 @@ public class TradePopUpController {
         return node;
     }
 
-    public void init(){
+    public void init() {
         root.getStylesheets().add("/de/uniks/pioneers/styles/SpinnerLowerArrowWidth.css");
         // setup spinners
-        List<Spinner> spinners= new ArrayList<>();
-        spinnerBox.getChildren().forEach(node -> spinners.add((Spinner) node));
-        spinnerBoxI.getChildren().forEach(node -> spinners.add((Spinner) node));
-        spinners.forEach(spinner -> {
+        Map<String, Spinner> spinners = new HashMap<>();
+        spinnerBoxOffer.getChildren().forEach(node -> spinners.put(node.getId(), (Spinner) node));
+        spinnerBoxGet.getChildren().forEach(node -> spinners.put(node.getId(), (Spinner) node));
 
-            spinnerValueFactory= new SpinnerValueFactory<Integer>() {
-                @Override
-                public void decrement(int steps) {
-                    int currentValue = getValue();
-                    if (currentValue>0){
-                        setValue(currentValue-1);
-                    }
-                }
-
-                @Override
-                public void increment(int steps) {
-                    int currentValue = getValue();
-                    setValue(currentValue+1);
-                }
-            };
-
-            spinnerValueFactory.setValue(0);
-            spinner.setValueFactory(spinnerValueFactory);
+        spinners.values().forEach(spinner -> {
+            spinner.getId().f
+            TradeSpinnerFactory factory = new TradeSpinnerFactory();
+            factory.setValue(0);
+            spinner.setValueFactory(factory);
         });
+
+
+        EventHandler<MouseEvent> eventHandler = event -> {
+
+        };
+
+        tradeWithBank.addEventHandler(MouseEvent.MOUSE_CLICKED, eventHandler);
+
+
     }
-
-
-
-
-
-
-
-
 
 
 }
