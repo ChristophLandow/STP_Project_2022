380766f4b26a8065c0bccc669fa1f99bdb2b0e38
Ferdinand Blum <uk044779@student.uni-kasssel.de>
2022-07-17 20:11:01 +0200

    chore/ deleted print to console commands

--- src/test/java/de/uniks/pioneers/controller/subcontroller/RobPlayerControllerTest.java
+++ src/test/java/de/uniks/pioneers/controller/subcontroller/RobPlayerControllerTest.java
@@ -70,7 +70,7 @@ class RobPlayerControllerTest extends ApplicationTest {
 
         sleep(1000);
         ListView<HBox> playerListView = lookup("#playerListView").query();
-        assertTrue(playerListView.isVisible());
+        assertFalse(playerListView.isVisible());
 
         playerListView.fireEvent(leftClick);
         type(KeyCode.DOWN);
