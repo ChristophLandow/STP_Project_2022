3cab75bf6854a6b46d5fd0e1da54e29af8c697c5
JannikNordmeyer <93387255+JannikNordmeyer@users.noreply.github.com>
2022-07-21 13:30:48 +0200

    feat: added basic rendering of hex tiles

--- src/main/java/de/uniks/pioneers/controller/MapEditorController.java
+++ src/main/java/de/uniks/pioneers/controller/MapEditorController.java
@@ -2,25 +2,41 @@ package de.uniks.pioneers.controller;
 
 
 import de.uniks.pioneers.Main;
+import de.uniks.pioneers.controller.subcontroller.HexTile;
+import de.uniks.pioneers.services.EditorManager;
+import de.uniks.pioneers.services.GameStorage;
+import de.uniks.pioneers.services.MapRenderService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Polygon;
 
 import javax.inject.Inject;
 import java.io.IOException;
+import java.util.List;
 
 
 public class MapEditorController implements Controller{
 
     @FXML
+    Pane scrollPaneAnchorPane;
+    @FXML
     Button buttonToMaps;
     @FXML
     Button buttonSave;
 
+    EditorManager editorManager;
+
+
 
     @Inject
-    public MapEditorController(){}
+    public MapEditorController(EditorManager editorManager){
+
+        this.editorManager = editorManager;
+
+    }
 
     @Override
     public void init() {
@@ -43,7 +59,34 @@ public class MapEditorController implements Controller{
             e.printStackTrace();
             return null;
         }
+        display(2);
+
         return parent;
+    }
+
+    private void display(int size){
+
+        int scale = 50;
+
+        List<HexTile> frame = this.editorManager.buildFrame(size, scale);
+
+        for(HexTile hexTile : frame){
+
+            Polygon tile = new Polygon();
+            tile.getPoints().addAll(0.0*scale, 1.0*scale,
+                    (Math.sqrt(3)/2)*scale,0.5*scale,
+                    (Math.sqrt(3)/2)*scale,-0.5*scale,
+                    0.0*scale,-1.0*scale,
+                    (-Math.sqrt(3)/2)*scale,-0.5*scale,
+                    (-Math.sqrt(3)/2)*scale,0.5*scale);
+
+            tile.setLayoutX(hexTile.x + this.scrollPaneAnchorPane.getPrefWidth() / 2);
+            tile.setLayoutY(-hexTile.y + this.scrollPaneAnchorPane.getPrefHeight() / 2);
+
+            this.scrollPaneAnchorPane.getChildren().add(tile);
+        }
+
+
     }
 
     public void toMaps(){
