0a0911135d8c763c4a1e05599db0f7dcf9909bd1
Aleyna Albayrak <uk076901@student.uni-kassel.de>
2022-07-23 18:13:19 +0200

    chore: create fxml fields

--- src/main/java/de/uniks/pioneers/controller/MapBrowserController.java
+++ src/main/java/de/uniks/pioneers/controller/MapBrowserController.java
@@ -11,7 +11,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.ListView;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
@@ -20,15 +23,23 @@ import java.io.IOException;
 
 @Singleton
 public class MapBrowserController implements Controller {
-    @Inject Provider<LobbyScreenController> lobbyScreenControllerProvider;
-    @Inject Provider<MapListController> mapListControllerProvider;
-
-    @Inject PrefService prefService;
-
+    @FXML public Pane mapPreviewPane;
+    @FXML public ImageView creatorImageView;
+    @FXML public Text createdOutputText;
+    @FXML public Text lastUpdatedOutputText;
+    @FXML public Text sizeOutputText;
+    @FXML public Text tilesOutputText;
+    @FXML public Text numbersOutputText;
+    @FXML public Text harborsOutputText;
+    @FXML public Text creatorNameOutputText;
     @FXML ScrollPane MapListScrollPane;
     @FXML ListView<HBox> mapListView;
+
+    @Inject Provider<LobbyScreenController> lobbyScreenControllerProvider;
+    @Inject Provider<MapListController> mapListControllerProvider;
+    @Inject PrefService prefService;
+
     @FXML private final App app;
-    private LobbyScreenController lobbyScreenController;
     private final StylesService stylesService;
     private MapListController mapListController;
 
@@ -38,8 +49,6 @@ public class MapBrowserController implements Controller {
         this.stylesService = stylesService;
     }
     
-
-
     @Override
     public void init() {
         String styleLocal = "/de/uniks/pioneers/styles/MapBrowser.css";
@@ -80,7 +89,7 @@ public class MapBrowserController implements Controller {
     }
 
     public void leaveToLobby(ActionEvent actionEvent) {
-        lobbyScreenController = lobbyScreenControllerProvider.get();
+        LobbyScreenController lobbyScreenController = lobbyScreenControllerProvider.get();
         app.show(lobbyScreenController);
     }
     public App getApp() {
