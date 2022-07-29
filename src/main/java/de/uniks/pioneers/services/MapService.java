2c2c8827c2c42f54cde35144b61db97faf527281
NSchintze <uk078202@student.uni-kassel.de>
2022-07-29 19:08:14 +0200

    feat: save harbors

--- src/main/java/de/uniks/pioneers/services/MapService.java
+++ src/main/java/de/uniks/pioneers/services/MapService.java
@@ -33,7 +33,7 @@ public class MapService {
     }
 
     public Observable<MapTemplate> saveMap(List<TileTemplate> tiles, List<HarborTemplate> harbors) {
-        return mapApiService.updateMap(currentMap._id(), new UpdateMapTemplateDto(null, null, tiles, harbors));
+        return mapApiService.updateMap(currentMap._id(), new UpdateMapTemplateDto(null, null, "haha", tiles, harbors));
     }
 
     public Observable<MapTemplate> createMap(String name, String icon, String description, List<TileTemplate> tiles, List<HarborTemplate> harbors) {
@@ -49,26 +49,25 @@ public class MapService {
     }
 
     public void updateOrCreateMap(List<EditTile> editTiles) {
-        List<HarborTemplate> harbors = new ArrayList<>();
-        harbors.add(new HarborTemplate(0, 0, 0, "grain", 1));
+        List<HarborTemplate> harborTemplates = this.getHarbors(editTiles);
         List<TileTemplate> tileTemplates = this.getTiles(editTiles);
         // if the map is null, create a new one
         if (this.getCurrentMap() != null) {
             //if the map belongs to the current player, update it
             if (this.getCurrentMap().createdBy().equals(userService.getCurrentUser()._id())) {
-                this.saveMap(tileTemplates, harbors)
+                this.saveMap(tileTemplates, harborTemplates)
                         .observeOn(FX_SCHEDULER)
                         .doOnError(err -> handleSaveError())
                         .subscribe();
             } else {
                 //if the map belongs to someone else, create a new one
-                this.createMap("Testus-Maximus-YEEEEE", null, "kein Bock auf Beschriebung", tileTemplates, harbors)
+                this.createMap("Testus-Maximus-YEEEEE", null, "kein Bock auf Beschriebung", tileTemplates, harborTemplates)
                         .observeOn(FX_SCHEDULER)
                         .doOnError(err -> handleSaveError())
                         .subscribe();
             }
         } else {
-            this.createMap("Testus-Maximus-ultra-deluxe", null, "kein Bock auf Beschriebung", tileTemplates, harbors)
+            this.createMap("Testus-Maximus-ultra-deluxe", null, "kein Bock auf Beschriebung", tileTemplates, harborTemplates)
                     .observeOn(FX_SCHEDULER)
                     .doOnError(err -> handleSaveError())
                     .subscribe();
@@ -89,8 +88,25 @@ public class MapService {
         return tiles;
     }
 
-    public List<HarborTemplate> getHarbors() {
-        return null;
+    public List<HarborTemplate> getHarbors(List<EditTile> editTiles) {
+        List<HarborTemplate> harbors = new ArrayList<>();
+        for (EditTile et : editTiles) {
+            //check if there is a harbor
+            if (!et.currentHarborType.equals("")){
+                int x = et.hexTile.q;
+                int y = et.hexTile.r;
+                int z = et.hexTile.s;
+                String type = et.currentHarborType.replace("harbour_", "");
+                //check if the harbour is a 3:1
+                if (type.equals("general")) {
+                    type = null;
+                }
+                int side = et.currentHarborSide;
+                HarborTemplate harborTemplate = new HarborTemplate(x, y, z, type, side);
+                harbors.add(harborTemplate);
+            }
+        }
+        return harbors;
     }
 
 
