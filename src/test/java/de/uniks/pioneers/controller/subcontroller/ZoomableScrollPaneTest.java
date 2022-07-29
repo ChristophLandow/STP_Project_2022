8524779341c4f63b55bbcc8762bf255ff1506b61
Giulio Pazzi <giulio.pazzi02@gmail.com>
2022-07-29 14:38:19 +0200

    fix: faster ZoomableScrollPaneTest

--- src/test/java/de/uniks/pioneers/controller/subcontroller/ZoomableScrollPaneTest.java
+++ src/test/java/de/uniks/pioneers/controller/subcontroller/ZoomableScrollPaneTest.java
@@ -48,21 +48,16 @@ class ZoomableScrollPaneTest extends ApplicationTest {
 
         //SignUpScreen
         WaitForAsyncUtils.waitForFxEvents();
-        verifyThat("#textFieldUserName", TextInputControlMatchers.hasText("TestUser"));
-        verifyThat("#passwordField", TextInputControlMatchers.hasText("12345678"));
         write("\t\t12345678\t\t\t");
         type(KeyCode.ENTER);
 
         //Dialog
         WaitForAsyncUtils.waitForFxEvents();
-        verifyThat(".information", Node::isVisible);
         type(KeyCode.ENTER);
 
         //LoginScreen
         WaitForAsyncUtils.waitForFxEvents();
         write("\t12345678\t");
-        verifyThat("#textFieldUserName", TextInputControlMatchers.hasText("TestUser"));
-        verifyThat("#passwordField", TextInputControlMatchers.hasText("12345678"));
         type(KeyCode.ENTER);
         write("\t");
         type(KeyCode.ENTER);
@@ -73,22 +68,13 @@ class ZoomableScrollPaneTest extends ApplicationTest {
         type(KeyCode.ENTER);
         clickOn("#gameNameTextField");
         write("TestGame\t");
-        verifyThat("#gameNameTextField", TextInputControlMatchers.hasText("TestGame"));
         write("12345678\t\t");
-        verifyThat("#passwordTextField", TextInputControlMatchers.hasText("12345678"));
         type(KeyCode.ENTER);
 
         //NewGameLobbyScreen
         WaitForAsyncUtils.waitForFxEvents();
-        verifyThat("#gameNameLabel", LabeledMatchers.hasText("TestGame"));
-        verifyThat("#passwordLabel", LabeledMatchers.hasText("12345678"));
-        TestModule.gameMemberSubject.onNext(new Event<>(".created", new Member("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "000", "001", false, "#ffffff", false)));
-        TestModule.gameMemberSubject.onNext(new Event<>(".created", new Member("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "000", "002", false, "#000000", false)));
-        TestModule.gameMemberSubject.onNext(new Event<>(".created", new Member("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "000", "003", false, "#888888", false)));
-        TestModule.gameSubject.onNext(new Event<>(".updated", new Game("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "000", "000", "000", 3, false , new GameSettings(2, 4, null, true, 0))));
-        TestModule.gameMemberSubject.onNext(new Event<>(".updated", new Member("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "000", "001", true, "#ffffff", false)));
-        TestModule.gameMemberSubject.onNext(new Event<>(".updated", new Member("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "000", "002", true, "#000000", false)));
-        TestModule.gameMemberSubject.onNext(new Event<>(".updated", new Member("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "000", "003", true, "#888888", false)));
+        TestModule.gameMemberSubject.onNext(new Event<>(".created", new Member("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "000", "001", true, "#ffffff", false)));
+        TestModule.gameSubject.onNext(new Event<>(".updated", new Game("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "000", "000", "000", 1, false , new GameSettings(2, 4, null, true, 0))));
         TestModule.mapTemplateSubject.onNext(new Event<>(".updated", new MapTemplate("","","id1","Test","","",0, null, null)));
         write("\t\t\t");
         type(KeyCode.SPACE);
