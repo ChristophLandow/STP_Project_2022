f010a2ec99b46b8d078702941c284c9c5352a347
Giulio Pazzi <giulio.pazzi02@gmail.com>
2022-08-04 12:25:39 +0200

    fix: AppTest rulesButton no nodes

--- src/test/java/de/uniks/pioneers/AppTest.java
+++ src/test/java/de/uniks/pioneers/AppTest.java
@@ -119,7 +119,7 @@ class AppTest extends ApplicationTest {
         type(KeyCode.SPACE);
         TestModule.gameChatSubject.onNext(new Event<>(".created", new MessageDto("2022-05-18T18:12:58.114Z", "2022-05-18T18:12:58.114Z", "004", "A", "Hallo Test Test")));
         WaitForAsyncUtils.waitForFxEvents();
-        sleep(2000);
+        sleep(3000);
         clickOn("#rulesButton");
         WaitForAsyncUtils.waitForFxEvents();
         clickOn("#settingsButton");
