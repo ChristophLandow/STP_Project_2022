ed2f4c6901095a60f8d6960f7d5ac311055a9649
Christoph Landow <clandow@web.de>
2022-08-05 12:08:26 +0200

    feat: added Methods to set Data in AchievementScreen

--- src/main/java/de/uniks/pioneers/controller/AchievementScreenController.java
+++ src/main/java/de/uniks/pioneers/controller/AchievementScreenController.java
@@ -2,22 +2,32 @@ package de.uniks.pioneers.controller;
 
 import de.uniks.pioneers.App;
 import de.uniks.pioneers.Main;
+import de.uniks.pioneers.model.Achievement;
 import de.uniks.pioneers.services.AchievementService;
+import de.uniks.pioneers.services.IngameService;
+import javafx.collections.ObservableMap;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.inject.Singleton;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+import static de.uniks.pioneers.GameConstants.*;
 
 @Singleton
 public class AchievementScreenController implements Controller {
 
+    private final IngameService ingameService;
     @FXML Label cityPlanerDateLabel;
     @FXML Label longestRoadDateLabel;
     @FXML Label seaBuilderDateLabel;
@@ -29,24 +39,50 @@ public class AchievementScreenController implements Controller {
     @FXML VBox seaBuilderBox;
     @FXML VBox wildWestBox;
     @FXML VBox chickenDinnerBox;
-
-
-
-
     private final AchievementService achievemetService;
     private final Provider<LobbyScreenController> lobbyScreenControllerProvider;
     private final App app;
+    private ObservableMap<String, Achievement> achievements;
 
     @Inject
-    public AchievementScreenController(App app, AchievementService achievementService, Provider<LobbyScreenController> lobbyScreenControllerProvider){
+    public AchievementScreenController(App app, IngameService ingameService, AchievementService achievementService, Provider<LobbyScreenController> lobbyScreenControllerProvider){
         this.achievemetService = achievementService;
+        this.ingameService = ingameService;
         this.lobbyScreenControllerProvider = lobbyScreenControllerProvider;
         this.app = app;
     }
 
     @Override
     public void init() {
+        achievements = achievemetService.getAchievements();
+        //set unlock dates and check-icon or unlock status
+        setAchievement(cityPlanerDateLabel, cityPlanerBox, achievements.get(CITY_ACHIEVEMENT));
+        setAchievement(longestRoadDateLabel, longestRoadBox, achievements.get(ROAD_ACHIEVEMENT));
+        setAchievement(wildWestDateLabel, wildWestBox, achievements.get(SETTLEMENT_ACHIEVEMENT));
+        setAchievement(chickenDinnerDateLabel, chickenDinnerBox, achievements.get(WINNER_ACHIEVEMENT));
+        //...for Harbor-Achievement extra, cause of lower progress lvl
+        if(achievements.get(HARBOR_ACHIEVEMENT).progress() == 50){
+            seaBuilderDateLabel.setText(achievements.get(HARBOR_ACHIEVEMENT).unlockedAt());
+            seaBuilderBox.getChildren().add(new ImageView());
+        } else {
+            Label statusLabel = new Label();
+            statusLabel.setText(String.valueOf(achievements.get(HARBOR_ACHIEVEMENT).progress()) + "/ 50");
+            cityPlanerDateLabel.setText("");
+            cityPlanerBox.getChildren().add(statusLabel);
+        }
 
+    }
+
+    public void setAchievement(Label dateLabel, VBox box, Achievement achievement){
+        if(achievement.progress() == 100){
+            dateLabel.setText(achievement.unlockedAt());
+            box.getChildren().add(new ImageView());
+        } else {
+            Label statusLabel = new Label();
+            statusLabel.setText(achievements.get(ROAD_ACHIEVEMENT).progress() + "/ 100");
+            dateLabel.setText("");
+            box.getChildren().add(statusLabel);
+        }
     }
 
     @Override
